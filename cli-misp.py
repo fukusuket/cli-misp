a1bd37af0c4efd849bae3f3212cbe8dd813a75f3
fukusuket <41001169+fukusuket@users.noreply.github.com>
2024-04-23 18:16:48 +0900

    fix: add update logic

--- cli-misp.py
+++ cli-misp.py
@@ -95,10 +95,8 @@ def build_misp_event(ui: UserInput, misp: PyMISP) -> (MISPEvent, bool):
     event = MISPEvent()
     if ui.event_uuid:
         existing_event = misp.get_event(ui.event_uuid, pythonify=True)
-        if "2" == str(existing_event.orgc):
+        if "BT 17" == existing_event.orgc.name:
             event = existing_event
-            event.delete_tag('workflow:state="complete"')
-            event.add_tag('workflow:state="draft"')
             is_new_event = False
         else:
             event.extends_uuid = ui.event_uuid
